6cd80a0b58e66a4e5e8cdf7a9857045d375de359
Ross Murray <rossmichaelm@gmail.com>
2019-09-24 15:12:47 +0100

    add husky to project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React, { useState } from 'react';
-import './App.css';
-import NewAnimal from './components/NewAnimal';
-import ShowAnimals from './components/ShowAnimals';
-import Emojis from './components/Emojis';
+import React, { useState } from "react";
+import "./App.css";
+import NewAnimal from "./components/NewAnimal";
+import ShowAnimals from "./components/ShowAnimals";
+import Emojis from "./components/Emojis";
 
 function App() {
+  const defaultAnimals = ["Tiger", "Dog", "Cat"];
 
-  const defaultAnimals = ['Tiger', 'Dog', 'Cat']
-
-  const [animals, setAnimals] = useState(defaultAnimals)
+  const [animals, setAnimals] = useState(defaultAnimals);
 
   const createAnimal = animal => event => {
-    setAnimals([...animals, animal])
-  }
+    setAnimals([...animals, animal]);
+  };
 
   const deleteAnimal = index => event => {
-    const animalsTemp = [...animals]
-    animalsTemp.splice(index, 1)
-    setAnimals(animalsTemp)
-  }
+    const animalsTemp = [...animals];
+    animalsTemp.splice(index, 1);
+    setAnimals(animalsTemp);
+  };
 
   return (
     <div>
